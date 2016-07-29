4fb11f2e9eb4cab04b1dd7bd2c9d1fbfeb7e95fc
Aaron-Bong <aaronbcy@gmail.com>
2016-07-29 18:15:41 +1200

    day6 - testing ts compile & implement app insight.

---
+++ lol.js
@@ -0,0 +1,5 @@
+/* When button is clicked by user,
+toggle hiding or showing the dropdown content */
+function dropDownFunc() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
